6769d6955fb087c72f46269575a87d6acb3e204e
Matt Derocher <mjamesderocher@gmail.com>
2022-05-08 12:50:22 -0400

    Updated Svelte and copied changes from FF-Clipper

---
+++ src/lib/Form.svelte
@@ -0,0 +1,36 @@
+<script>
+  import String from '$lib/fields/String.svelte'
+  import Number from '$lib/fields/Number.svelte'
+  import Fieldset from '$lib/fields/Fieldset.svelte'
+  import Radio from '$lib/fields/Radio.svelte'
+  export let customFields = {}
+  export let schema
+  //Create empty data structure if none provided?
+  export let data
+  export let changeHandler
+  export let path
+
+  const fields = {
+    string: String,
+    number: Number,
+    object: Fieldset,
+    radio: Radio,
+    ...customFields,
+  }
+</script>
+
+{#each Object.entries(schema.properties) as [key, field]}
+  {#if field.display !== 'private'}
+    {#if fields[field.component] || fields[field.type]}
+      <svelte:component
+        this={field.component ? fields[field.component] : fields[field.type]}
+        {key}
+        required={schema.required ? schema.required.includes(key) : false}
+        {...field}
+        {data}
+        {changeHandler}
+        path={path ? [...path, key] : [key]}
+      />
+    {/if}
+  {/if}
+{/each}
---
+++ src/routes/index.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Form from '$lib/Form.svelte'
+  import { writable } from 'svelte/store'
+
+  const schema = {
+    $schema: 'https://json-schema.org/draft/2020-12/schema',
+    $id: 'https://example.com/product.schema.json',
+    title: 'Product',
+    description: "A product from Acme's catalog",
+    type: 'object',
+    properties: {
+      product: {
+        description: 'Enter some text',
+        type: 'string',
+      },
+      product2: {
+        type: 'string',
+        //"minLength": 2,
+        //"maxLength": 3
+      },
+      latitude: {
+        type: 'number',
+        minimum: -90,
+        maximum: 90,
+        multipleOf: 0.01,
+      },
+    },
+    required: ['product'],
+  }
+
+  const data = writable({
+    product: '',
+    product2: '',
+    latitude: 0,
+  })
+
+  const changeHandler = (e, path) => {
+    $data = {
+      ...$data,
+      [path[0]]: e,
+    }
+  }
+
+  $: console.log($data)
+</script>
+
+<div>
+  {#if schema.title}
+    <h1>{schema.title}</h1>
+  {/if}
+  <Form {schema} data={$data} {changeHandler} />
+</div>
